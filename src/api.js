fccd3b7f4d519d9c28c6075cce342d19987c660a
MemeWorld15 <164206815+MemeWorld15@users.noreply.github.com>
2025-05-29 02:19:43 -0600

    Update api.js
    Agregado de administrador

--- src/api.js
+++ src/api.js
@@ -20,3 +20,34 @@ export const registrarUsuario = (formData) =>
     body: formData,
     mode: 'cors'
   });
+
+// CATEGORÍAS
+export const fetchCategorias = () => fetch(`${BASE_URL}/categorias`).then(res => res.json())
+export const crearNuevaCategoria = (nombre) => {
+  const formData = new FormData()
+  formData.append('nombre', nombre)
+  return fetch(`${BASE_URL}/categorias`, { method: 'POST', body: formData })
+}
+export const actualizarCategoria = (id, nombre) => {
+  const formData = new FormData()
+  formData.append('nombre', nombre)
+  return fetch(`${BASE_URL}/categorias/${id}`, { method: 'PUT', body: formData })
+}
+export const eliminarCategoriaPorId = (id) =>
+  fetch(`${BASE_URL}/categorias/${id}`, { method: 'DELETE' })
+
+// PRODUCTOS
+export const fetchProductos = () => fetch(`${BASE_URL}/productos`).then(res => res.json())
+export const crearProducto = (formData) =>
+  fetch(`${BASE_URL}/productos`, { method: 'POST', body: formData })
+export const actualizarProducto = (id, formData) =>
+  fetch(`${BASE_URL}/productos/${id}`, { method: 'PUT', body: formData })
+export const eliminarProductoPorId = (id) =>
+  fetch(`${BASE_URL}/productos/${id}`, { method: 'DELETE' })
+
+// ÓRDENES
+export const fetchOrdenes = () => fetch(`${BASE_URL}/ordenes`).then(res => res.json())
+
+// EMPLEADOS
+export const crearEmpleado = (formData) =>
+  fetch(`${BASE_URL}/empleados`, { method: 'POST', body: formData })
