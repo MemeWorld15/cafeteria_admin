d86d9043e21cd333152afb6853b6fd6ec7ad3a93
MemeWorld15 <164206815+MemeWorld15@users.noreply.github.com>
2025-06-10 00:53:19 -0600

    Update probbar Inventario.vue

--- src/views/Inventario.vue
+++ src/views/Inventario.vue
@@ -125,7 +125,7 @@ const obtenerInventario = async () => {
 }
 
 // ➕ Agregar insumo
-const agregarInsumo = async () => {
+const agregarNuevoInsumo = async () => {
   const form = new FormData()
   form.append('nombre', nuevoInsumo.value.nombre)
   form.append('cantidad', nuevoInsumo.value.cantidad)
